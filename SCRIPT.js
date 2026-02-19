88f6af748688246a6932d79964dff5e5fc10cc10
shubhamthokare18 <shubhamthokare.eidiko@gmail.com>
2026-02-19 12:49:52 +0530

    13/03/2026

---
+++ SCRIPT.js
@@ -0,0 +1,80 @@
+var a;
+const dulha = "Lab";
+var dulhan = "Laby";
+console.log(a);
+a = 12;
+var b = a;
+b = b + 2;
+let a = 12;
+var a = [12, 13];
+var b = a;
+b.pop();
+var abcd = Navi;
+var abcd = "alnjkhwfishenbi";
+var abcd = {};
+for (var i=0; i<11; i++) {
+    console.log(i);
+}
+var a=12;
+while(a<20){
+    a++;
+}
+function helobolo(a,b,c){
+    console.log(a,b,c);
+    console.log("hello");
+}
+helobolo(12,13,14);
+var arr=[1,2,3,4,5,6,7];
+arr[0];
+arr.push(8);
+arr.pop();
+arr.unshift(0);
+arr.shift();
+arr.splice(2,1);
+var a={};
+var a=12;
+let b=12;
+function abcd () {
+    var a = 12;
+    function abc () {
+        var b = 13;
+    }
+}
+var a = [1, 2, 3, 4, 5];
+var b = [...a];
+b.pop();
+var obj = {name: "harsh"};
+var copyobj = {...obj};
+switch(1){
+    case 1:
+        break;
+        case 2:
+            break;
+            default:
+}
+var a = [1,2,3,4,5,6,7,8,9];
+a.forEach(function(val){
+    console.log(val+2);
+})
+var obj = {
+    name: "harsh",
+    age: 24,
+    city: "bhopal"
+}
+for(var key in obj){
+    console.log(key, obj[key]);
+}
+var a = 120;
+do{
+    console.log("hey");
+    a++;
+}
+while(a<15)
+    setTimeout(function(){
+        console.log("2 second baad chala");
+    }, 2000);
+
+function abcd(a){
+    a();
+}    
+abcd(function(){console.log("hey");});
